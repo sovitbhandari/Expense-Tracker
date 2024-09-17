9a2516969e14e2519d615e3ac2990e49def35e5d
Sovit Bhandari <149540959+Sovit-Bhandari@users.noreply.github.com>
2024-09-17 13:04:00 -0400

    Add files via upload

---
+++ backend/models/UserModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        default: 'User', // Added default name for clarity
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: 'male', // Set default avatar as male
+    }
+});
+
+// Hash password before saving (runs only if the password is modified or new)
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Compare entered password with hashed password in the database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
