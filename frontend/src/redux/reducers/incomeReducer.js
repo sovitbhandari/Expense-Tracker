62aa9c569e08fd329c288a205031c314a493d01a
Sovit Bhandari <149540959+sovitbhandari@users.noreply.github.com>
2025-02-08 11:56:21 -0500

    Update incomeReducer.js

--- frontend/src/redux/reducers/incomeReducer.js
+++ frontend/src/redux/reducers/incomeReducer.js
@@ -1,1 +1,14 @@
-..
+import { createSlice } from '@reduxjs/toolkit';
+
+const incomeSlice = createSlice({
+    name: 'income',
+    initialState: [],
+    reducers: {
+        setIncomes: (state, action) => action.payload,
+        addIncome: (state, action) => [...state, action.payload],
+        removeIncome: (state, action) => state.filter(income => income.id !== action.payload),
+    }
+});
+
+export const { setIncomes, addIncome, removeIncome } = incomeSlice.actions;
+export default incomeSlice.reducer;
