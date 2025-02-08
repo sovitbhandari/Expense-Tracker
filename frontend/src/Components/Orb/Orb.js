741e9ba5adb0fd22c15c5e0880629636db460139
Sovit Bhandari <149540959+sovitbhandari@users.noreply.github.com>
2025-02-08 12:44:58 -0500

    Update Orb.js

--- frontend/src/Components/Orb/Orb.js
+++ frontend/src/Components/Orb/Orb.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { useWindowSize } from '../../utils/useWindowSize';
 
-// Define keyframes and styled component outside the functional component
-const moveOrb = keyframes`
+// Define keyframes dynamically
+const moveOrb = (width, height) => keyframes`
   0% {
     transform: translate(0, 0);
   }
   50% {
-    transform: translate(var(--orb-width), var(--orb-height));
+    transform: translate(${width / 4}px, ${height / 4}px);
   }
   100% {
     transform: translate(0, 0);
@@ -20,24 +20,18 @@ const OrbStyled = styled.div`
   height: 70vh;
   position: absolute;
   border-radius: 50%;
-  margin-left: -37vh;
-  margin-top: -37vh;
+  margin-left: -35vh;
+  margin-top: -35vh;
   background: linear-gradient(180deg, #F56692 100%, #F2994A 100%);
   filter: blur(400px);
-  animation: ${moveOrb} 15s alternate linear infinite;
   z-index: 1;
 `;
 
 function Orb() {
   const { width, height } = useWindowSize();
+  const animation = moveOrb(width, height);
 
-  // Set CSS variables for dynamic styling
-  const style = {
-    '--orb-width': `${width}px`,
-    '--orb-height': `${height / 2}px`,
-  };
-
-  return <OrbStyled style={style} />;
+  return <OrbStyled style={{ animation: `${animation} 15s alternate linear infinite` }} />;
 }
 
 export default Orb;
