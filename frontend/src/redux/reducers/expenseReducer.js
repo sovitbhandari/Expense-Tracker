e575ee8aa618c33391d9e63ca54f74b1af7c8010
Sovit Bhandari <149540959+sovitbhandari@users.noreply.github.com>
2025-02-08 11:55:59 -0500

    Update expenseReducer.js

--- frontend/src/redux/reducers/expenseReducer.js
+++ frontend/src/redux/reducers/expenseReducer.js
@@ -1,1 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
 
+const expenseSlice = createSlice({
+    name: 'expenses',
+    initialState: [],
+    reducers: {
+        setExpenses: (state, action) => action.payload,
+        addExpense: (state, action) => [...state, action.payload],
+        removeExpense: (state, action) => state.filter(expense => expense.id !== action.payload),
+    }
+});
+
+export const { setExpenses, addExpense, removeExpense } = expenseSlice.actions;
+export default expenseSlice.reducer;
