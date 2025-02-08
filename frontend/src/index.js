a9856c957b52a9921d57e4e5509ad895e3ac5820
Sovit Bhandari <149540959+sovitbhandari@users.noreply.github.com>
2025-02-08 12:09:30 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';  // Import Redux Provider
-import store from './store/store';  // Import Redux store
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
 
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
-    <Provider store={store}>  {/* Wrap app with Redux Provider */}
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
