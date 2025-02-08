b041f679196e2365d19363447e6a91e07e1a48a8
Sovit Bhandari <149540959+sovitbhandari@users.noreply.github.com>
2025-02-08 12:13:31 -0500

    Create store.js

---
+++ frontend/src/redux/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import incomeReducer from './incomeSlice';
+import expenseReducer from './expenseSlice';
+import userReducer from './userSlice';
+
+const store = configureStore({
+  reducer: {
+    income: incomeReducer,
+    expense: expenseReducer,
+    user: userReducer
+  }
+});
+
+export default store;
